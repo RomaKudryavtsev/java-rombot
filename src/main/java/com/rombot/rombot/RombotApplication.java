3e19c8b054123ad8c5d64e24df5097838d37bd5b
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2023-07-22 22:03:10 +0300

    add swagger

--- src/main/java/com/rombot/rombot/RombotApplication.java
+++ src/main/java/com/rombot/rombot/RombotApplication.java
@@ -1,12 +1,21 @@
 package com.rombot.rombot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.rombot.rombot")
+@ComponentScan(basePackages = "com.rombot.rombot")
+@OpenAPIDefinition(info = @Info(
+		title = "rombot",
+		version = "1.0",
+		description = "Reactive WhatsApp message sending tool"))
 public class RombotApplication {
+	//To see Swagger UI use this link: http://localhost:8080/swagger-ui
 	public static void main(String[] args) {
 		SpringApplication.run(RombotApplication.class, args);
 	}
--- src/main/java/com/rombot/rombot/controller/SendingController.java
+++ src/main/java/com/rombot/rombot/controller/SendingController.java
@@ -2,6 +2,8 @@ package com.rombot.rombot.controller;
 
 import com.rombot.rombot.dto.ResultDto;
 import com.rombot.rombot.service.SendingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -13,11 +15,16 @@ import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/send")
+@Tag(name = "sending_template", description = "Enable sending message stream")
 @RequiredArgsConstructor
 @Slf4j
 public class SendingController {
     private final SendingService service;
 
+    @Operation(
+            summary = "Enable sending message stream",
+            description = "This will subscribe you on the reactive stream"
+    )
     @PostMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ResultDto> sendMessage(@RequestParam("template") String templateName) {
         return service.startSending(templateName)
